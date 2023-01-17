cdd32f41680506101c0fe7da0721cd8b90bb80f1
minilogic <box4xyz@gmail.com>
2023-01-17 22:29:48 +0000

    add GPL2

--- xprintf.c
+++ xprintf.c
@@ -1,3 +1,31 @@
+/*-------------------------------------------------------------------------
+   xprintf.c - Tiny formatted output routine for use with sdcc/mcs51
+
+   Copyright (C) 2022, Eugene Chaban, box4xyz@gmail.com
+
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License 
+   along with this library; see the file COPYING. If not, write to the
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+
+   As a special exception, if you link this library with other files,
+   some of which are compiled with SDCC, to produce an executable,
+   this library does not by itself cause the resulting executable to
+   be covered by the GNU General Public License. This exception does
+   not however invalidate any other reasons why the executable file
+   might be covered by the GNU General Public License.
+-------------------------------------------------------------------------*/
+
 /* Formatted string output:
   -------------------------
   xprintf("%c", 'a');             "a"
@@ -15,6 +43,7 @@
   xprintf("%p", code_pointer);    "C:0x1234"
   xprintf("%p", data_pointer);    "I:0x1234"
 */
+
 void xprintf (__code const char *fmt, ...) __reentrant __naked
 {
      fmt; // r0-pointer, r1-radix, r2-width, r3-numctr, r4..r7-number(low..high)
